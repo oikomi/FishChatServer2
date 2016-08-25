2d939b62ebededf73db4078fded249fcc466c21c
oikomi <miaohonghit@gmail.com>
2016-08-25 20:20:12 +0800

    init

--- gateway/gateway.go
+++ gateway/gateway.go
@@ -6,10 +6,8 @@ import (
 	"github.com/golang/glog"
 	// "github.com/golang/protobuf/proto"
 	"github.com/oikomi/FishChatServer2/gateway/conf"
-	"github.com/oikomi/FishChatServer2/libnet"
+	// "github.com/oikomi/FishChatServer2/libnet"
 	// mybinary "github.com/oikomi/FishChatServer2/libnet/binary"
-	"io"
-	"io/ioutil"
 )
 
 func init() {
@@ -23,38 +21,5 @@ func main() {
 		glog.Error("conf.Init() error: ", err)
 		panic(err)
 	}
-	// server, err := libnet.Serve("tcp", conf.Conf.Server.Addr, libnet.Packet(2, 1024*1024, 1024, binary.BigEndian, TestCodec{}))
-	server, err := libnet.Serve("tcp", conf.Conf.Server.Addr, TestCodec{}, 1024)
-	if err != nil {
-		glog.Error("libnet.Serve error: ", err)
-		panic(err)
-	}
-	glog.Info("server start: ", server.Listener().Addr().String())
-	for {
-		session, err := server.Accept()
-		if err != nil {
-			glog.Error("server.Accept error: ", err)
-			break
-		}
-		go func() {
-			addr := session.Conn().RemoteAddr().String()
-			glog.Info("client ", addr, " connected")
-			for {
-				println("#####")
-				var msg []byte
-				if err = session.Receive(&msg); err != nil {
-					glog.Error("session.Receive error: ", err)
-					break
-				}
-				println("--2---")
-				println(string(msg))
-				glog.Info("receive msg : ", string(msg))
-				// if err = session.Send(msg); err != nil {
-				// 	glog.Error("session.Send error: ", err)
-				// 	break
-				// }
-			}
-			//println("client", addr, "closed")
-		}()
-	}
+
 }
