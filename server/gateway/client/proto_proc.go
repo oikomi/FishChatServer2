865db4af8aba701eb9c6a26ff12444132e2dd88c
miaohong <miaohong@jk.cn>
2016-11-16 11:20:52 +0800

    init

--- server/gateway/client/proto_proc.go
+++ server/gateway/client/proto_proc.go
@@ -5,11 +5,15 @@ import (
 	"github.com/oikomi/FishChatServer2/common/ecode"
 	"github.com/oikomi/FishChatServer2/protocol/external"
 	"github.com/oikomi/FishChatServer2/server/gateway/job"
+	"math/rand"
 )
 
 func (c *Client) procReqAccessServer(reqData []byte) (err error) {
 	var addr string
-	accessServerList := job.AccessServerList
+	var accessServerList []string
+	for _, v := range job.AccessServerList {
+		accessServerList = append(accessServerList, v.IP)
+	}
 	if len(accessServerList) == 0 {
 		if err = c.Session.Send(&external.ResSelectAccessServerForClient{
 			Cmd:     external.ReqAccessServerCMD,
@@ -20,9 +24,7 @@ func (c *Client) procReqAccessServer(reqData []byte) (err error) {
 		}
 		return
 	}
-	for _, v := range accessServerList {
-		addr = v.IP
-	}
+	addr = accessServerList[rand.Intn(len(accessServerList))]
 	if err = c.Session.Send(&external.ResSelectAccessServerForClient{
 		Cmd:     external.ReqAccessServerCMD,
 		ErrCode: ecode.OK.Uint32(),
