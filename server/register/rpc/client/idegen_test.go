32952e69f051c9d2d48659d53e6ef1e6138f77b7
miaohong <miaohong@jk.cn>
2017-02-12 20:35:15 +0800

    init

--- server/register/rpc/client/idegen_test.go
+++ server/register/rpc/client/idegen_test.go
@@ -1,18 +1,18 @@
 package client
 
-import (
-	"github.com/oikomi/FishChatServer2/protocol/rpc"
-	"testing"
-)
+// import (
+// 	"github.com/oikomi/FishChatServer2/protocol/rpc"
+// 	"testing"
+// )
 
-func TestGetUUID(t *testing.T) {
-	idgenRPC, err := NewIdgenRPCCli()
-	if err != nil {
-		t.Error(err)
-	}
-	r, err := idgenRPC.GetUUID(&rpc.Snowflake_NullRequest{})
-	if err != nil {
-		t.Fatalf("could not get next value: %v", err)
-	}
-	t.Logf("%b", r.Uuid)
-}
+// func TestGetUUID(t *testing.T) {
+// 	idgenRPC, err := NewIdgenRPCCli()
+// 	if err != nil {
+// 		t.Error(err)
+// 	}
+// 	r, err := idgenRPC.GetUUID(&rpc.Snowflake_NullRequest{})
+// 	if err != nil {
+// 		t.Fatalf("could not get next value: %v", err)
+// 	}
+// 	t.Logf("%b", r.Uuid)
+// }
