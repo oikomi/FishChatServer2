49209c6a531a698629f32b2c89a6172da4559bf4
miaohong <miaohong@jk.cn>
2017-02-15 17:10:17 +0800

    init

--- server/manager/dao/dao.go
+++ server/manager/dao/dao.go
@@ -1,30 +1,19 @@
 package dao
 
-import (
-	"github.com/golang/glog"
-)
-
 type Dao struct {
-	Redis   *Redis
-	MongoDB *MongoDB
-	HBase   *HBase
-	Mysql   *Mysql
+	Redis *Redis
+	HBase *HBase
+	Mysql *Mysql
 }
 
 func NewDao() (dao *Dao) {
-	m, err := NewMongoDB()
-	if err != nil {
-		glog.Error(err)
-		panic(err)
-	}
 	redis := NewRedis()
 	h := NewHBase()
 	mysql := NewMysql()
 	dao = &Dao{
-		Redis:   redis,
-		MongoDB: m,
-		HBase:   h,
-		Mysql:   mysql,
+		Redis: redis,
+		HBase: h,
+		Mysql: mysql,
 	}
 	return
 }
--- server/manager/dao/mongodb.go
+++ server/manager/dao/mongodb.go
@@ -1,34 +1,34 @@
 package dao
 
-import (
-	"github.com/golang/glog"
-	"github.com/oikomi/FishChatServer2/common/dao/mongodb"
-	commmodel "github.com/oikomi/FishChatServer2/common/model"
-	"github.com/oikomi/FishChatServer2/server/manager/conf"
-	"gopkg.in/mgo.v2/bson"
-)
+// import (
+// 	"github.com/golang/glog"
+// 	"github.com/oikomi/FishChatServer2/common/dao/mongodb"
+// 	commmodel "github.com/oikomi/FishChatServer2/common/model"
+// 	"github.com/oikomi/FishChatServer2/server/manager/conf"
+// 	"gopkg.in/mgo.v2/bson"
+// )
 
-type MongoDB struct {
-	m *mongodb.MongoDB
-}
+// type MongoDB struct {
+// 	m *mongodb.MongoDB
+// }
 
-func NewMongoDB() (mdb *MongoDB, err error) {
-	m, err := mongodb.NewMongoDB(conf.Conf.MongoDB.MongoDB)
-	if err != nil {
-		glog.Error(err)
-	}
-	mdb = &MongoDB{
-		m: m,
-	}
-	return
-}
+// func NewMongoDB() (mdb *MongoDB, err error) {
+// 	m, err := mongodb.NewMongoDB(conf.Conf.MongoDB.MongoDB)
+// 	if err != nil {
+// 		glog.Error(err)
+// 	}
+// 	mdb = &MongoDB{
+// 		m: m,
+// 	}
+// 	return
+// }
 
-func (m *MongoDB) GetOfflineMsg(uid int64) (res []*commmodel.OfflineMsg, err error) {
-	c := m.m.Session.DB(conf.Conf.MongoDB.DB).C(conf.Conf.MongoDB.OfflineMsgCollection)
-	res = make([]*commmodel.OfflineMsg, 0)
-	if err = c.Find(bson.M{"targetuid": uid}).All(&res); err != nil {
-		glog.Error(err)
-	}
-	glog.Info(res)
-	return
-}
+// func (m *MongoDB) GetOfflineMsg(uid int64) (res []*commmodel.OfflineMsg, err error) {
+// 	c := m.m.Session.DB(conf.Conf.MongoDB.DB).C(conf.Conf.MongoDB.OfflineMsgCollection)
+// 	res = make([]*commmodel.OfflineMsg, 0)
+// 	if err = c.Find(bson.M{"targetuid": uid}).All(&res); err != nil {
+// 		glog.Error(err)
+// 	}
+// 	glog.Info(res)
+// 	return
+// }
